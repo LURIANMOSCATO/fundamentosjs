1fd53b19612a5bd34223d2f4b63ad2965ba1cfbf
Desenvolvimento <lurianmoscato@gmail.com>
2023-04-03 19:51:05 -0300

    exemplos da aula 1

---
+++ type-boolean.js
@@ -0,0 +1,15 @@
+const primeiroNumero = 5;
+const segundoNumero = 5;
+let cadastroAtivado = true;
+
+//console.log(primeiroNumero === segundoNumero); 
+
+const texto1 = "Alura";
+const texto2 = "alura";
+
+console.log(texto1 === texto2);
+
+
+// === é comparaçãoe e retorna verdadeiro ou falso
+
+// exemplo de uso prático: senha do usuário no banco é igual a inserida agora?
---
+++ type-string.js
@@ -0,0 +1,13 @@
+const texto1 = "Olá, Mundo!";
+const texto2 = "Olá, Mundo!";
+const senha = "senhaSuperSegura456!"; // tudo entre aspas considera uma string
+const StringDeNumeros = "2345";
+const citacao = "Meu nome é "; 
+const meuNome = "Lurian";
+console.log (citacao + meuNome);
+
+// concatenação (+)
+
+
+//template string ou template literal
+
