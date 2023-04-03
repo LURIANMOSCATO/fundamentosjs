a56b9eed3622513fec58c890690d3bc29f3a46a6
Desenvolvimento <lurianmoscato@gmail.com>
2023-04-03 20:47:36 -0300

    var e convesoes

---
+++ conversoes.js
@@ -0,0 +1,14 @@
+// tipo de dado
+// booleanos
+
+// conversao implicita (permita convertemos um tipo de dado em outro, number por string ex)
+
+const numero = 456;
+const numeroString =Number ("456");
+
+//Number()
+//String()
+
+console.log(numero + numeroString);
+
+// conversão explícita
---
+++ trusthy-false.js
@@ -0,0 +1,22 @@
+// boolean 
+
+const usuarioLogado = true; // se usuário logado for igual a 'true' ele pode fazer algumas coisas no sitema(ex)
+const contapaga = false; 
+
+//trufhy or faly
+
+
+//null = representa nada, vazio
+// undefined
+
+let minhaVar;
+let varNull = null;
+
+console.log(minhaVar)
+console.log(varNull)
+
+let numero = 3;
+let texto = "Alura";
+
+console.log(typeof minhaVar);
+console.log(typeof varNull); //null = object ocorre devido a um bug, que se concertado pode quebrar a linguagem;
